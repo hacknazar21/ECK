ef3f0cdc21e82a1d7b8119eb145d9edadf7a18af
Nazar Cooltaev <nazarcooltaev@192-168-0-123.local>
2023-01-09 10:38:41 +0600

    fixes bugs

--- components/profile/Team/Create-Team.js
+++ components/profile/Team/Create-Team.js
@@ -1,35 +1,14 @@
 import React from "react";
 import ProfilePageLayout from "../../../layouts/ProfilePageLayout";
-import Aside from "../../common/Aside/Aside";
-import Link from "next/link";
 import CTContent from "./CTContent";
+import ProfileMenu from "../ProfileMenu";
 
 function Team(props) {
   return (
     <section className="page__my-profile notifications">
       <div className="notifications__container">
         <ProfilePageLayout>
-          <Aside>
-            <li className={"aside-menu__item"}>
-              <Link href={"/profile/executor/my-profile"}>
-                <a className="aside-menu__link">Мой профиль</a>
-              </Link>
-            </li>
-            <li className={"aside-menu__item"}>
-              <Link href={"/profile/executor/notifications"}>
-                <a className="aside-menu__link">Уведомления</a>
-              </Link>
-            </li>
-            <li className={"aside-menu__item active"}>
-              <div className="aside-menu__link">Команды</div>
-            </li>
-            <li className={"aside-menu__item"}>
-              <Link href={"/profile/executor/projects"}>
-                <a className="aside-menu__link">Мои проекты</a>
-              </Link>
-            </li>
-          </Aside>
-
+          <ProfileMenu />
           <CTContent />
         </ProfilePageLayout>
       </div>
