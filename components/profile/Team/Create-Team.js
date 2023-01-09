226724176090bdaa5cacf152cac1b59bd268bddc
Nazar Cooltaev <nazarcooltaev@192-168-0-123.local>
2023-01-09 10:43:39 +0600

    fixes bugs

--- components/profile/Team/Create-Team.js
+++ components/profile/Team/Create-Team.js
@@ -2,13 +2,16 @@ import React from "react";
 import ProfilePageLayout from "../../../layouts/ProfilePageLayout";
 import CTContent from "./CTContent";
 import ProfileMenu from "../ProfileMenu";
+import Aside from "../../common/Aside/Aside";
 
 function Team(props) {
   return (
     <section className="page__my-profile notifications">
       <div className="notifications__container">
         <ProfilePageLayout>
-          <ProfileMenu />
+          <Aside>
+            <ProfileMenu />
+          </Aside>
           <CTContent />
         </ProfilePageLayout>
       </div>
